583779b8a50e646fc604d0b37ca73c2726907346
David Alexandre <davidalexandre@live.com>
2020-06-13 23:52:41 -0300

    Atualizacao

--- una_quest/prova/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from questoes.models import Questao
-from django.contrib.auth.models import User
-
-class Prova(models.Model):
-    questoes = models.ManyToManyField(Questao)
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
--- una_quest/respostas/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Resposta(models.Model):
-    descricao = models.CharField(max_length = 400)
-    flegCerto = models.BooleanField(default=False)
